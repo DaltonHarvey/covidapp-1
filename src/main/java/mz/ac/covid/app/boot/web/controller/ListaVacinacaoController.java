4c7d038deeaab4831fb23e0a39bf731110cb3bd5
helderjosue <helderjosuemata@gmail.com>
2021-07-17 02:54:00 +0000

    adding listaVacinacaoController

---
+++ src/main/java/mz/ac/covid/app/boot/web/controller/ListaVacinacaoController.java
@@ -0,0 +1,10 @@
+package mz.ac.covid.app.boot.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/listas")
+public class ListaVacinacaoController {
+
+}
