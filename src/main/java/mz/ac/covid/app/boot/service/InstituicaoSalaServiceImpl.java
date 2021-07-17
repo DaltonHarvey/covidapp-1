3d6dbd65ca8156dc0c0e88626da1d65189fda7e6
helderjosue <helderjosuemata@gmail.com>
2021-07-17 14:41:50 +0000

    refactor instSalaService

--- src/main/java/mz/ac/covid/app/boot/service/InstituicaoSalaServiceImpl.java
+++ src/main/java/mz/ac/covid/app/boot/service/InstituicaoSalaServiceImpl.java
@@ -3,10 +3,14 @@ package mz.ac.covid.app.boot.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import mz.ac.covid.app.boot.dao.InstituicaoSalaDao;
 import mz.ac.covid.app.boot.domain.InstituicaoSala;
 
+@Service
+@Transactional(readOnly = false)
 public class InstituicaoSalaServiceImpl implements InstituicaoSalaService {
 
     @Autowired
